d0a70738a37f17e120d46516374b64c4e6e76910
AgustinSajama <sajamaagustin2007@gmail.com>
2026-04-15 14:58:36 -0300

    python tp ejer1 - eje5

---
+++ python_tp/hola.py
@@ -0,0 +1,39 @@
+def eje1():
+    n1 = int(input("ingrese un numero: "))
+    n2 = int(input("ingrese un segundo numero: "))
+    r = n1 + n2
+    print("el resultado es: ", r)
+#eje1()
+
+def eje2():
+    n1 = int(input("ingrese un numero: "))
+    n2 = int(input("ingrese un segundo numero: "))
+    n3 = int(input("ingrese un tercer numero: "))
+    n4 = int(input("ingrese un cuarto numero: "))
+    r = n1 + n2 + n3 + n4
+    print("el resultado es: ", r)
+#eje2()
+
+def eje3():
+    n1 = int(input("ingrese un num: "))
+    n2 = int(input("ingrese otro: "))
+    r = n1 * n2
+    print("la superficie es: ", r)
+#eje3()
+
+def eje4():
+    n = float(input("ingrese un número con decimales: "))
+    A = n * n
+    print("el Area del cuadrado es: ", A)
+#eje4()
+
+def eje5():
+    s = int(input("ingrese una cantidad de segundos: "))
+    m = int(input("ingrese una cantidad de minutos: "))
+    h = int(input("ingrese una cantidad de horas: "))
+    hs = h * 3600
+    ms = m * 60
+    R = hs + ms + s
+    print("hay un total de ", R, " segundos")
+#eje5()
+    
