a5baa26e7677ae133f2b4500a4ed3abfadc830c0
DarioBenjaminMontero <dariomontero2008@gmail.com>
2026-04-20 15:08:06 -0300

    completado

--- python_tp/hola.py
+++ python_tp/hola.py
@@ -62,8 +62,35 @@ def eje8():
     print("El resultado es: ", R, "%")
 #eje8()
 
+
 def eje9():
+    F = int(input("ingrese una fecha: "))
+    A = F % 10000
+    M = (F // 10000) % 100
+    MD = (F // 1000000) % 100
+    D = F // 1000000
+    print("el año es: ", A)
+    print("el mes es: ", M)
+    print("el día es: ", D)
 #eje9()
 
 def eje10():
-    
+    EX = int(input("Ingresa nota de los examenes principales: "))
+    TPS = int(input("ingresa nota de los trabajos practicos: "))
+    EXI = int(input("ingresa nota de los examenes integradores: "))
+    r1 = EXI * 0.5
+    r2 = TPS * 0.2
+    r3 = EX * 0.3
+    RE = r1 + r2 + r3
+    print("el rendimiento es de: ",RE)
+#eje10()
+
+def eje11():
+    SM = 5500
+    CV = 200
+    VA = 0.05
+    NV = int(input("ingrese el numero de autos vendidos: "))
+    VV = int(input("ingrese el valor de venta de la unidad: "))
+    R = SM + (CV * NV) + (VA * VV * NV)
+    print("el salario total del vendedor es: ", R)   
+#eje11()
